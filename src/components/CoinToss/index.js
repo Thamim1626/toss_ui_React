14b9edc28db678d7c4405b90a35d72b55bba4860
Thamim Ansari <thamimansari1626@gmail.com>
2023-11-29 01:03:04 +0000

    Finished the first task its push commit"

--- src/components/CoinToss/index.js
+++ src/components/CoinToss/index.js
@@ -2,10 +2,51 @@ import {Component} from 'react'
 import './index.css'
 
 class CoinToss extends Component {
+  state = {
+    head: 0,
+    tail: 0,
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/heads-img.png',
+    total: 0,
+  }
+
+  onClick = () => {
+    const randomtoss = Math.floor(Math.random() * 2)
+    if (randomtoss === 0) {
+      this.setState(prevState => ({
+        head: prevState.head + 1,
+        total: prevState.total + 1,
+      }))
+      this.setState({
+        imgUrl: 'https://assets.ccbp.in/frontend/react-js/heads-img.png',
+      })
+    } else {
+      this.setState(prevState => ({
+        tail: prevState.head + 1,
+        total: prevState.total + 1,
+      }))
+      this.setState({
+        imgUrl: 'https://assets.ccbp.in/frontend/react-js/tails-img.png',
+      })
+    }
+  }
+
   render() {
+    const {head, tail, imgUrl, total} = this.state
     return (
-      <div>
-        <p>test</p>
+      <div className="container">
+        <div className="card">
+          <h1 className="heading">Coin Toss Game</h1>
+          <p className="des">Heads (or) Tails</p>
+          <img src={imgUrl} alt="toss result" className="coin-img" />
+          <button className="toss-button" onClick={this.onClick} type="button">
+            Toss Coin
+          </button>
+          <div className="result">
+            <p className="result">Total: {total}</p>
+            <p className="result">heads: {head}</p>
+            <p className="result">Tails: {tail}</p>
+          </div>
+        </div>
       </div>
     )
   }
