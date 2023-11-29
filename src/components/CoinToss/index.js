e0fd7c202ba88700d3270703e4d133a18d916974
Thamim Ansari <thamimansari1626@gmail.com>
2023-11-29 00:16:29 +0000

    Node package install and coin tosdd rendering successfully

--- src/components/CoinToss/index.js
+++ src/components/CoinToss/index.js
@@ -1,1 +1,14 @@
-// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class CoinToss extends Component {
+  render() {
+    return (
+      <div>
+        <p>test</p>
+      </div>
+    )
+  }
+}
+
+export default CoinToss
